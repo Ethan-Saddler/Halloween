14c69874f873512523e684e1d7ccc168529d6eea
Ethan <ethansaddlerm@gmail.com>
2022-11-01 10:25:30 -0400

    accomlpice done

--- Accomplice.java
+++ Accomplice.java
@@ -1,1 +1,18 @@
+public class Accomplice extends PersonOfInterest {
+    private boolean canCode;
 
+    public Accomplice(Color hairColor, int proximity, RubberDuck rubberDuck, boolean canCode) {
+        super(hairColor, proximity, rubberDuck);
+        this.canCode = canCode;
+    }
+    public String toString() {
+        return String.format("Possible Accomplice: %s They %s have the ability to hack doorbells.", super.toString(), canCode ? "do" : "do not");
+    }
+    public boolean equals(Object obj) {
+        // make sure classes being different is not messing up method
+        if (super.equals(obj) == true) {
+            Accomplice accomplice = (Accomplice) obj;
+            return this.canCode == accomplice.canCode;
+        }
+    }
+}
--- Suspect.java
+++ Suspect.java
@@ -1,1 +1,25 @@
+public class Suspect extends PersonOfInterest {
+    private int height = 150;
 
+    public Suspect(Color hairColor, int proximity, RubberDuck rubberDuck, int height) {
+        super(hairColor, proximity, rubberDuck);
+
+        if (height > 150) {
+            this.height = height;
+        }
+    }
+    public Suspect(Color hairColor, int proximity) {
+        super(hairColor, proximity);
+        this.height = 160;
+    }
+    public String toString() {
+        return String.format("Possible Suspect: %s They were observed to be %d cm tall.", super.toString(), height);
+    }
+    public boolean equals(Object obj) {
+    // make sure classes being different is not messing up method
+        if (super.equals(obj) == true) {
+            Suspect suspect = (Suspect) obj;
+            return this.height == suspect.height;
+        }
+    }
+}
