a2ac72b03c3c26bea0508dab38ffe3773c8eb899
Ethan <ethansaddlerm@gmail.com>
2022-11-01 20:12:55 -0400

    police station work

--- PoliceStation.java
+++ PoliceStation.java
@@ -1,1 +1,27 @@
+public class PoliceStation {
+    private String name;
+    private ArrayList<Suspect> suspects;
+    private ArrayList<Accomplice> accomplices;
 
+    public PoliceStation(String name) {
+        if (name == null) {
+            this.name = "Station 1331";
+        } else {
+            this.name = name;
+        }
+        this.suspects = new ArrayList<Suspect>();
+        this.accomplice = new ArrayList<Accomplice>();
+    }
+    public void addPersonOfInterest(PersonOfInterest poi) {
+        if (poi.getName() == "Suspect") {
+            Suspect suspect = (Suspect) poi;
+            suspects.add(suspect);
+        } else {
+            Accomplice accomplice = (Accomplice) poi;
+            accomplices.add(accomplice);
+        }
+    }
+    public void sortSuspects() {
+        
+    }
+}
